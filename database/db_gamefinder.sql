c7b2cd51abcfd1798f817795d63e1e6870bcefa8
arielpatatas <arielpatatas@users.noreply.github.com>
2020-02-06 10:07:16 +0800

    Update db_gamefinder.sql

--- database/db_gamefinder.sql
+++ database/db_gamefinder.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2019 at 07:30 AM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Generation Time: Feb 06, 2020 at 03:06 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,19 +22,64 @@ SET time_zone = "+00:00";
 -- Database: `db_gamefinder`
 --
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', 'f925916e2754e5e03f75dd58a5733251', '2017-05-13 11:18:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `availability`
+--
+
+CREATE TABLE `availability` (
+  `availability_id` int(11) NOT NULL,
+  `space_id` int(11) NOT NULL,
+  `mon-in` time NOT NULL,
+  `mon-out` time NOT NULL,
+  `tue-in` time NOT NULL,
+  `tue_out` time NOT NULL,
+  `wed-in` time NOT NULL,
+  `wed-out` time NOT NULL,
+  `thu-in` time NOT NULL,
+  `thu-out` time NOT NULL,
+  `fri-in` time NOT NULL,
+  `fri-out` time NOT NULL,
+  `sat-in` time NOT NULL,
+  `sat-out` time NOT NULL,
+  `sun-in` time NOT NULL,
+  `sun-out` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `booking`
 --
 
--- CREATE TABLE `booking` (
---   `booking id` int(11) NOT NULL,
---   `space_id` int(11) NOT NULL,
---   `user_id` int(11) NOT NULL,
---   `host_id` int(11) NOT NULL,
---   `date_time` datetime NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `booking` (
+  `booking id` int(11) NOT NULL,
+  `space_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `host_id` int(11) NOT NULL,
+  `date_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,24 +87,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `feed`
 --
 
--- CREATE TABLE `feed` (
---   `feed_id` int(11) NOT NULL,
---   `user_id` int(11) NOT NULL,
---   `message` varchar(255) NOT NULL,
---   `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
--- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `feed`
---
-
--- INSERT INTO `feed` (`feed_id`, `user_id`, `message`, `date_added`) VALUES
--- (3, 4, 'cute', '2018-12-13 14:54:57'),
--- (4, 4, 'ko', '2018-12-13 15:16:01'),
--- (5, 4, 'hehe', '2018-12-13 15:16:21'),
--- (6, 5, 'sasad', '2019-01-03 10:06:00'),
--- (7, 5, 'asdsad', '2019-01-03 11:29:01'),
--- (8, 5, 'looking workspace sa olongapo', '2019-01-08 17:05:53');
+CREATE TABLE `feed` (
+  `feed_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -67,16 +100,57 @@ SET time_zone = "+00:00";
 -- Table structure for table `notification`
 --
 
--- CREATE TABLE `notification` (
---   `notification_id` int(11) NOT NULL,
---   `to_user` int(11) NOT NULL,
---   `from_user` int(11) NOT NULL,
---   `type` varchar(255) NOT NULL,
---   `date_time` datetime NOT NULL,
---   `body` varchar(255) NOT NULL,
---   `workspace_id` int(11) NOT NULL,
---   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
--- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `notification` (
+  `notification_id` int(11) NOT NULL,
+  `to_user` int(11) NOT NULL,
+  `from_user` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `date_time` datetime NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `workspace_id` int(11) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `space_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `isVerify` tinyint(1) NOT NULL DEFAULT '1',
+  `workspace_image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lat` double NOT NULL,
+  `lang` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stores`
+--
+
+INSERT INTO `stores` (`id`, `name`, `lat`, `lang`) VALUES
+(1, 'CHOAS Supermart', 14.8402345, 120.287124),
+(2, 'SM Olongapo', 14.8265356, 120.2828903);
 
 -- --------------------------------------------------------
 
@@ -90,74 +164,71 @@ CREATE TABLE `user` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `contactnum` int(11) NOT NULL
+  `contactnum` int(25) NOT NULL,
+  `Type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `email`, `password`, `contactnum`) VALUES
-(2, 'Ariel', 'Bonina', 'boninaariel@gmail.com', 'arielsuperpogi', '09089840734'),
-(4, 'ver', 'legaspi', 'ver@g.com', '$2b$05$yTN5uYlAPUfq4eyj0ajpXuqTdgmkUu8cm8c09LWDu.K1nUdcLlayq', 29531321),
-(5, 'shane', 'royena', 'royenashane@gmail.com', '$2b$05$JUXtFvaJ9wzhhL1KNYJbNeOw0MvgxCMIuLNlIK4wr3lfRkblDv1Ze', 2147483647);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `workspace`
---
-
-CREATE TABLE `workspace` (
-  `user_id` int(11) NOT NULL,
-  `picture` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  'quantity' varchar(255) NOT NULL,
-  `isVerify` tinyint(1) NOT NULL DEFAULT '0',
-  `workspace_image` varchar(255) NOT NULL,
-  `productname` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `workspace`
---
-
-INSERT INTO `workspace` (`user_id`, `picture`, `price`, `type`, `quantity`, `isVerify`, `workspace_image`, `productname`, `description`) VALUES
-(1, 'picture here', '17,000', 'GAMING CONSOLE', '1', 1, 'http://localhost:3000/images/workspace/5a9dq7vsjpcp6w46.jpg', 'PLAYSTATION4', 'medyo used')
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `email`, `password`, `contactnum`, `Type`) VALUES
+(19, 'aqeqe', 'wqeqe', 'test', '$2b$05$H3bqreaoz671qddOgE1JnOKxlp.JcPcJQWE.WFJHSqOg4YQBegdXi', 12312312, ''),
+(20, 'test', 'test', 'test', '$2b$05$9B/7b2s34hvf57hny98QWuSJwkrrFPcDTIMlLAQ7RNZqx/LrIWGpq', 123, ''),
+(21, 'test', 'test', 'test', '$2b$05$uN2PSKCzAqnaagQvstHr8.XaLLoQC47qDySRSPF4P3EsPuC/7mbYu', 123, ''),
+(22, 'Ariel', 'Boninaa', 'test2', '$2b$05$z4FZrbPAFOE7YMjzWkpzuuOwfWmHCWIxKAgFIbCIIAGGmrE.dm3Tu', 123, '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `availability`
 --
--- ALTER TABLE `availability`
---   ADD PRIMARY KEY (`availability_id`);
+ALTER TABLE `availability`
+  ADD PRIMARY KEY (`availability_id`);
 
 --
 -- Indexes for table `booking`
--- --
--- ALTER TABLE `booking`
---   ADD PRIMARY KEY (`booking id`);
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`booking id`);
 
 --
 -- Indexes for table `feed`
 --
--- ALTER TABLE `feed`
---   ADD PRIMARY KEY (`feed_id`),
---   ADD KEY `user_id` (`user_id`);
+ALTER TABLE `feed`
+  ADD PRIMARY KEY (`feed_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `notification`
 --
--- ALTER TABLE `notification`
---   ADD PRIMARY KEY (`notification_id`),
---   ADD KEY `to_user` (`to_user`),
---   ADD KEY `from_user` (`from_user`),
---   ADD KEY `workspace_id` (`workspace_id`);
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notification_id`),
+  ADD KEY `to_user` (`to_user`),
+  ADD KEY `from_user` (`from_user`),
+  ADD KEY `workspace_id` (`workspace_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`space_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `store_id` (`store_id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -166,51 +237,56 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indexes for table `workspace`
---
-ALTER TABLE `workspace`
-  ADD PRIMARY KEY (`space_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `availability`
 --
--- ALTER TABLE `availability`
---   MODIFY `availability_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `availability`
+  MODIFY `availability_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `booking`
 --
--- ALTER TABLE `booking`
---   MODIFY `booking id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `booking`
+  MODIFY `booking id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `feed`
 --
--- ALTER TABLE `feed`
---   MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `feed`
+  MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notification`
 --
--- ALTER TABLE `notification`
---   MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `notification`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `space_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `workspace`
---
-ALTER TABLE `products`
-  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
@@ -219,22 +295,23 @@ ALTER TABLE `products`
 --
 -- Constraints for table `feed`
 --
--- ALTER TABLE `feed`
---   ADD CONSTRAINT `feed_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `feed`
+  ADD CONSTRAINT `feed_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `notification`
 --
--- ALTER TABLE `notification`
---   ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`to_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
---   ADD CONSTRAINT `notification_ibfk_2` FOREIGN KEY (`from_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
---   ADD CONSTRAINT `notification_ibfk_3` FOREIGN KEY (`workspace_id`) REFERENCES `workspace` (`space_id`);
+ALTER TABLE `notification`
+  ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`to_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notification_ibfk_2` FOREIGN KEY (`from_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notification_ibfk_3` FOREIGN KEY (`workspace_id`) REFERENCES `product` (`space_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `workspace`
+-- Constraints for table `product`
 --
-ALTER TABLE `workspace`
-  ADD CONSTRAINT `workspace_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
